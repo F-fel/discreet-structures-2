711283474245329fa211be7a9bd4d9edab0d671e
Simoneau <lucas.simoneau@gmail.com>
2019-11-03 14:55:02 -0500

    add class order

---
+++ Tp1/src/Tp1/Order.java
@@ -0,0 +1,32 @@
+package Tp1;
+
+public class Order {
+
+    int TotalWeight(Objet objet){
+        int totalWeight=0;
+
+        //blabla bla
+
+
+        return totalWeight;
+
+    }
+
+
+    Time(Objet objet){
+        int k=0;
+
+        if(RobotX){
+            k=1+TotalWeight(objet); //mettre variable robot
+        }
+        else if (RobotY){
+            k=1.5+0.6*TotalWeight(objet); //mettre variable robot
+        }
+        else if (RobotZ){
+            k=2.5+0.2*TotalWeight(objet); //mettre variable robot
+        }
+        int t=k*
+    }
+
+
+}
