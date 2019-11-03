8c6d69bb4b7e45bcbf6ca5f40ea8e339ad01d520
Eredra <arthurleboiteux@hotmail.com>
2019-11-03 16:05:50 -0500

    ajout prendre commande et ajouter commande

--- Tp1/src/Tp1/Order.java
+++ Tp1/src/Tp1/Order.java
@@ -12,6 +12,25 @@ public class Order {
 
     }
 
+    public int prendreCommande(int A, int B, int C){
+
+        int commande [] = new int[3];
+
+        commande[0] = A;
+        commande[1] = B;
+        commande[2] = C;
+
+        return commande;
+
+    }
+
+    public void afficherCommande(int [] commande){
+
+        System.out.println("Nombre d'objets A : "+ commande[0]);
+        System.out.println("Nombre d'objets B : "+ commande[1]);
+        System.out.println("Nombre d'objets C : "+ commande[2]);
+    }
+
 
     Time(Objet objet){
         int k=0;
