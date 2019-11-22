629c68f019f280cc4fcc45becbda5e0335b35354
faelfa <farid.elfakhry@polymtl.ca>
2019-11-22 13:09:56 -0500

    model et controlleur left, vue is done

--- TP2/src/Tp2/GraphicInterface.java
+++ TP2/src/Tp2/GraphicInterface.java
@@ -1,25 +1,65 @@
 package Tp2;
 import javax.swing.*;
-public class GraphicInterface {
-    public void run() {
-        JLabel intro = new JLabel("Bienvenu au TP2!",SwingConstants.CENTER);
-        JFrame f = new JFrame();//creating instance of JFrame
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GraphicInterface extends JFrame {
+    public GraphicInterface() {
+        super("TP4");
+        JLabel welcome = new JLabel("Bienvenue au Tp2 de Log2410");
+        setLayout(new GridLayout(5,1));
         JButton init = new JButton("Initialiser le programme");
+        init.addActionListener(new InitListener());
         JButton research = new JButton("Rechercher un élément");
+        research.addActionListener(new researchListener());
         JButton showCart = new JButton("Afficher le panier");
+        showCart.addActionListener(new CartListener());
         JButton close = new JButton("Fermer la session");
-        init.setBounds(100,150 , 300, 40);//x axis, y axis, width, height
-        research.setBounds(100,250,300,40);
-        showCart.setBounds(100,350,300,40);
-        close.setBounds(100,450,300,40);
-        f.add(init);
-        f.add(research);
-        f.add(showCart);
-        f.add(close);
-        f.add(intro);
-        f.setSize(500, 800);
-        f.setLayout(null);//using no layout managers
-        f.setVisible(true);//making the frame visible
+        close.addActionListener(new closeListener());
+        welcome.setVisible(true);
+        add(welcome);
+        add(init);
+        add(research);
+        add(showCart);
+        add(close);
+        setSize(300, 400);
+        setVisible(true);//making the frame visible
     }
 
+    private class researchListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            /*
+             * TODO
+             * */
+        }
+    }
+
+    private class InitListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            /*
+             * TODO
+             * */
+        }
+    }
+
+    private class CartListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            /*
+             * TODO
+             * */
+        }
+    }
+
+    private class closeListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            /*
+             * TODO
+             * */
+        }
+    }
 }
--- TP2/src/Tp2/Main.java
+++ TP2/src/Tp2/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         GraphicInterface userInterface = new GraphicInterface();
-        userInterface.run();
     }
 
 }
