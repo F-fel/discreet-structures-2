a71d939bc5c1aeb8cdff4c14f097d741ba0f1dc1
SpongeyBob <44511714+SpongeyBob@users.noreply.github.com>
2019-11-23 14:07:02 -0500

    added fully functional Automate

--- TP2/src/Tp2/Automate.java
+++ TP2/src/Tp2/Automate.java
@@ -1,30 +1,26 @@
 package Tp2;
 
-import java.util.LinkedList;
-
+import java.util.ArrayList;
 public class Automate {
-
-    /***
-     * 3 methodes that uses no filter
-     */
-    public String[] getNames(LinkedList<Objet> inv){
-        String[] retval = new String[inv.size()];
-        int i =0; //retval index
-        for (Objet o:inv) retval[i++] = o.getName();
-        return retval;
-    }
-    public Hex[] getIds(LinkedList<Objet> inv){
-        Hex[] retVal = new Hex[inv.size()];
-        int i =0;
-        for (Objet o : inv)
-            retVal[i++] = o.getID();
-        return retVal;
-    }
-    public ObjectType[] getTypes(LinkedList<Objet> inv){
-        ObjectType[] retVal = new ObjectType[inv.size()];
-        int i =0;
-        for (Objet o : inv)
-            retVal[i++] = o.getType();
-        return retVal;
-    }
+  private State[] states;
+  Automate( State[] stateList){
+    states = stateList;
+  }
+  public ArrayList<String> filter(String[] data){
+    ArrayList<String> retval = new ArrayList<>();
+    for(String s : data){
+      if(s.length() >= states.length){
+        char[] sArray  = s.toCharArray();
+        int i = 0; //index
+        while(states[i].validate(sArray[i])){
+          i++;
+          if(i == states.length){
+            retval.add(s);
+            break;
+          }
+        } 
+      }
+    }//end for
+    return retval;
+  }
 }
